43bd83cc815ba62c691abaa70bb6f37c94b7b315
AmundBerge <amundpamund@icloud.com>
2025-01-20 14:41:22 +0100

    Added sliding piece attack targets and pawn pushes

--- include/movegen.h
+++ include/movegen.h
@@ -11,6 +11,11 @@
 
 std::vector<uint16_t> getPlayerMoves(GameState board); 
 
+uint64_t slidingPieceAttacks(GameState board, int pc, int sq);
 
+uint64_t whiteSinglePawnPushTargets(GameState board);
+uint64_t whiteDoublePawnPushTargets(GameState board);
+uint64_t blackSinglePawnPushTargets(GameState board);
+uint64_t blackDoublePawnPushTargets(GameState board);
 
 #endif
--- src/movegen.cpp
+++ src/movegen.cpp
@@ -21,7 +21,7 @@ std::vector<uint16_t> getPlayerMoves(GameState board){
                 int target = q & 0x3F;
                 uint16_t move = (start << 10) | (target << 4) | 0x0;
                 moves.push_back(move);
-                destinations &= destinations - 1;
+                destinations &= destinations - 1; // kan skrives som for-lokke 
             }
             knightsCopy &= knightsCopy - 1;
         }
@@ -30,4 +30,33 @@ std::vector<uint16_t> getPlayerMoves(GameState board){
 
     return moves;
 
+}
+
+uint64_t slidingPieceAttacks(GameState board, int piece, int square){ 
+    uint64_t ts = pieceAttacks[piece][square];
+    for (uint64_t bb = board.occupiedSquares & blockers[piece][square]; bb != 0; bb &= (bb - 1)){
+        int target = __builtin_ctzll(bb);
+        ts &= ~behind[square][target];
+    }
+    return ts;
+}
+
+uint64_t whiteSinglePawnPushTargets(GameState board){
+    return (board.whitePawns << 8) & ~board.occupiedSquares;
+}
+
+uint64_t whiteDoublePawnPushTargets(GameState board){
+    const uint64_t rank4 = 0x00000000FF000000;
+    uint64_t singlePushes = whiteSinglePawnPushTargets(board);
+    return (singlePushes << 8) & ~board.occupiedSquares & rank4;
+}
+
+uint64_t blackSinglePawnPushTargets(GameState board){
+    return (board.blackPawns >> 8) & ~board.occupiedSquares;
+}
+
+uint64_t blackDoublePawnPushTargets(GameState board){
+    const uint64_t rank4 = 0x000000FF00000000;
+    uint64_t singlePushes = blackSinglePawnPushTargets(board);
+    return (singlePushes >> 8) & ~board.occupiedSquares & rank4;   
 }
